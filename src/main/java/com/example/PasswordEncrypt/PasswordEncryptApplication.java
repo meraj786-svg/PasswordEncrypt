1a2ac0e4dbe0d8154679967c3094f7a6e1ed4530
meraj786-svg <mrm081266@gmail.com>
2025-05-18 16:45:34 +0530

    finalprojectcsd

---
+++ src/main/java/com/example/PasswordEncrypt/PasswordEncryptApplication.java
@@ -0,0 +1,13 @@
+package com.example.PasswordEncrypt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordEncryptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordEncryptApplication.class, args);
+	}
+
+}
